2ad31a1240da40ee9d31e20de7c0c307e1ab70b1
Teodor Tunev <teodortunev@gmail.com>
2014-10-23 17:23:32 +0300

    Fix for post price bug

--- GassBuddy.Web/LoggedUser/PostPrice.aspx.cs
+++ GassBuddy.Web/LoggedUser/PostPrice.aspx.cs
@@ -11,11 +11,11 @@ namespace GassBuddy.Web.LoggedUser
 {
     public partial class PostPrice : System.Web.UI.Page
     {
-        private GassBuddyData data;        
+        private GassBuddyData data;
         public GasStation currentGasStation;
 
         protected void Page_Load(object sender, EventArgs e)
-        {           
+        {
             data = new GassBuddyData();
 
             var queryStrObject = HttpUtility.ParseQueryString(this.Page.ClientQueryString);
@@ -23,7 +23,7 @@ namespace GassBuddy.Web.LoggedUser
 
             this.DropDownListFuelTypes.DataSource = Enum.GetNames(typeof(FuelType));
 
-            currentGasStation = data.GasStations.All().FirstOrDefault(x => x.Id == gasStationId);   
+            currentGasStation = data.GasStations.All().FirstOrDefault(x => x.Id == gasStationId);
 
             var gasStationCoords = currentGasStation.GeoLocation.Split(';');
             this.StationLat.Text = gasStationCoords[0];
@@ -34,14 +34,14 @@ namespace GassBuddy.Web.LoggedUser
         protected void ButtonSubmitPrice_Click(object sender, EventArgs e)
         {
             if (!this.Page.IsValid)
-            {                
+            {
                 return;
             }
             this.UpdateGasStationPrices();
             this.UpdateUserHistory();
             this.data.SaveChanges();
 
-            this.SuccessMessage.InnerText = "Data information successfully saved";            
+            this.SuccessMessage.InnerText = "Data information successfully saved";
             this.PostPricePageContent.Visible = false;
             this.ButtonBackToHome.Visible = true;
             this.userForm.Visible = false;
@@ -49,20 +49,31 @@ namespace GassBuddy.Web.LoggedUser
 
         private void UpdateGasStationPrices()
         {
-            var dieselPrice = decimal.Parse(this.TextBoxDieselPrice.Text);
-            var petrolPrice = decimal.Parse(this.TextBoxPetrolPrice.Text);
-            var lpgPrice = decimal.Parse(this.TextBoxLpgPrice.Text);
-            
-            this.currentGasStation.DieselPrice = dieselPrice;
-            this.currentGasStation.PetrolPrice = petrolPrice;
-            this.currentGasStation.LpgPrice = lpgPrice;
+            if (this.TextBoxDieselPrice.Text.Length != 0)
+            {
+                var dieselPrice = decimal.Parse(this.TextBoxDieselPrice.Text);
+                this.currentGasStation.DieselPrice = dieselPrice;
+            }
+
+            if (this.TextBoxPetrolPrice.Text.Length != 0)
+            {
+
+                var petrolPrice = decimal.Parse(this.TextBoxPetrolPrice.Text);
+                this.currentGasStation.PetrolPrice = petrolPrice;
+            }
+            if (this.TextBoxLpgPrice.Text.Length != 0)
+            {
+                var lpgPrice = decimal.Parse(this.TextBoxLpgPrice.Text);
+
+                this.currentGasStation.LpgPrice = lpgPrice;
+            }
         }
 
         private void UpdateUserHistory()
-        {            
+        {
             var username = this.User.Identity.Name;
             var userId = this.data.Users.All().FirstOrDefault(x => x.UserName == username).Id;
-            var pricePerLitter =  decimal.Parse(this.TextBoxPricePerLitter.Text);
+            var pricePerLitter = decimal.Parse(this.TextBoxPricePerLitter.Text);
             var fuelQuantity = decimal.Parse(this.TextBoxFuelQuantity.Text);
             var totalPrice = pricePerLitter * fuelQuantity;
             var gasStationId = this.currentGasStation.Id;
