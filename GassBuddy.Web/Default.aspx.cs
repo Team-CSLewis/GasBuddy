39f67de0e9b31bbf9b7409559063bbf3a0b26d96
Zhiko Mitov <mitov90@gmail.com>
2014-10-23 13:12:53 +0300

    Home page - listing Stations with paging, statistics - average price, station count

--- GassBuddy.Web/Default.aspx.cs
+++ GassBuddy.Web/Default.aspx.cs
@@ -1,17 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-
-namespace GassBuddy.Web
+﻿namespace GassBuddy.Web
 {
-    public partial class _Default : Page
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+
+    using GassBuddy.Data;
+
+    public partial class Default : Page
     {
+        private GassBuddyData data;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            data = new GassBuddyData();
+            if (Page.IsPostBack)
+            {
+                return;
+            }
 
+            GridViewStations.DataSource =
+                data.GasStations.All().OrderBy(s => s.DieselPrice + s.PetrolPrice + s.LpgPrice).ToList();
+            GridViewStations.DataBind();
+
+            var gasStationsCount = Enumerable.Count(data.GasStations.All());
+            LiteralStationsCount.Text = gasStationsCount.ToString(CultureInfo.InvariantCulture);
+
+            var avgDieselPrice = data.GasStations.All().Average(s => s.DieselPrice);
+            AvgDiselPrice.Text = avgDieselPrice.ToString("C", CultureInfo.CreateSpecificCulture("Bg-bg"));
+
+            var avgLpgPrice = data.GasStations.All().Average(s => s.LpgPrice);
+            AvgLPGPrice.Text = avgLpgPrice.ToString("C", CultureInfo.CreateSpecificCulture("Bg-bg"));
+
+            var avgPetrolPrice = data.GasStations.All().Average(s => s.PetrolPrice);
+            AvgPetrolPrice.Text = avgPetrolPrice.ToString("C", CultureInfo.CreateSpecificCulture("Bg-bg"));
         }
+
+        protected void GridViewStations_OnPageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridViewStations.PageIndex = e.NewPageIndex;
+            GridViewStations.DataSource =
+                data.GasStations.All().OrderBy(s => s.DieselPrice + s.PetrolPrice + s.LpgPrice).ToList();
+            GridViewStations.DataBind();
+        }
+
     }
 }
